1a8c6889f4f00253f7b814ca77df8d2390be8fa3
natakumtest <nkumasinskaya@gmail.com>
2024-12-08 16:28:26 +0300

    add code formatting

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticketPrice =29_875;
+        int ticketPrice = 29_875;
         int rubMile = 20;
         int bonusMile = ticketPrice / rubMile;
 
-        System.out.println("Если стоимость билета: " + ticketPrice + " рублей, то");
+        System.out.println("Если стоимость билета: " + ticketPrice + " рублей, то-----------");
         System.out.println("Количество бонусных миль: " + bonusMile);
 
     }
